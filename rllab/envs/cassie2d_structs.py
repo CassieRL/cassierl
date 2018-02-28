18cc182e84837e96de59b1389160d0fec7469167
Phi Luu <philectron@users.noreply.github.com>
2018-02-28 00:04:31 -0800

    Add doc strings for cassie2d_structs

--- rllab/envs/cassie2d_structs.py
+++ rllab/envs/cassie2d_structs.py
@@ -1,11 +1,17 @@
 import ctypes as ct
+import numpy as np
+
 
 class ControllerTorque(ct.Structure):
     _fields_ = [('torques', ct.c_double*6)]
 
+
 class ControllerForce(ct.Structure):
+    """OSC = Operation Space Control"""
+
     _fields_ = [('left_force', ct.c_double*3),
                 ('right_force', ct.c_double*3)]
+
 
 class ControllerOsc(ct.Structure):
     _fields_ = [('body_xdd', ct.c_double*2),
@@ -13,7 +19,13 @@ class ControllerOsc(ct.Structure):
                 ('right_xdd', ct.c_double*2),
                 ('pitch_add', ct.c_double)]
 
+
 class StateGeneral(ct.Structure):
+    """
+    arrays of size 3 consist of x, z, phi
+    arrays of size 5 consist of hip, knee, ankle, toe, rod
+    """
+
     _fields_ = [('base_pos', ct.c_double*3),
                 ('base_vel', ct.c_double*3),
                 ('left_pos', ct.c_double*5),
@@ -21,7 +33,10 @@ class StateGeneral(ct.Structure):
                 ('right_pos', ct.c_double*5),
                 ('right_vel', ct.c_double*5)]
 
+
 class StateOperationalSpace(ct.Structure):
+    """This class has the same structure as StateGeneral."""
+
     _fields_ = [('body_x', ct.c_double*3),
                 ('body_xd', ct.c_double*3),
                 ('left_x', ct.c_double*3),
@@ -29,9 +44,11 @@ class StateOperationalSpace(ct.Structure):
                 ('right_x', ct.c_double*3),
                 ('right_xd', ct.c_double*3)]
 
-class InterfaceStructConverter():
 
-    def operational_state_to_array(self,state):
+class InterfaceStructConverter():
+    """Converting arrays to structures and structures to arrays"""
+
+    def operational_state_to_array(self, state):
         s = np.zeros((18,), dtype=np.double)
         for i in range(3):
             s[i] = state.body_x[i]
@@ -42,7 +59,7 @@ class InterfaceStructConverter():
             s[i+15] = state.right_xd[i]
         return s
 
-    def general_state_to_array(self,state):
+    def general_state_to_array(self, state):
         s = np.zeros((26,), dtype=np.double)
         for i in range(3):
             s[i] = state.base_pos[i]
@@ -54,7 +71,7 @@ class InterfaceStructConverter():
             s[i+21] = state.right_vel[i]
         return s
 
-    def array_to_general_state(self,s):
+    def array_to_general_state(self, s):
         state = StateGeneral()
         for i in range(3):
             state.base_pos[i] = s[i]
@@ -66,7 +83,7 @@ class InterfaceStructConverter():
             state.right_vel[i] = s[i+21]
         return state
 
-    def array_to_operational_action(self,action):
+    def array_to_operational_action(self, action):
         a = ControllerOsc()
         for i in range(2):
             a.body_xdd[i] = action[i]
@@ -74,5 +91,3 @@ class InterfaceStructConverter():
             a.right_xdd[i] = action[4+i]
         a.pitch_add = action[6]
         return a
-
-
