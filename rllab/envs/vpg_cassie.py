ea8fd31a86068574882ca91d30bdb57879561a8c
Phi Luu <philectron@users.noreply.github.com>
2018-05-06 13:34:53 -0700

    Enable plotting (?) & fix quote style

--- rllab/envs/vpg_cassie.py
+++ rllab/envs/vpg_cassie.py
@@ -15,10 +15,10 @@ def run_task(*_):
     env = normalize(Cassie2dEnv())
 
     if load_policy:
-        filename = "123"
+        filename = '123'
         data = joblib.load(filename)
         policy = data['policy']
-        print("Loading Pretrained Policy ...............................")
+        print('Loading Pretrained Policy ...............................')
     else:
         # use Gaussian maximum likelihood policy
         policy = GaussianMLPPolicy(
@@ -37,11 +37,11 @@ def run_task(*_):
         baseline=baseline,
         batch_size=10000,
         max_path_length=1000,  # dt = (1/2000)*n, where n is Step(n)
-        n_itr=2000,
+        n_itr=1000,
         discount=0.99,
         step_size=0.005,      # default was 0.01
         # Uncomment both lines (this and the plot parameter below) to enable plotting
-        plot=False,
+        plot=True,
         use_gpu=True
     )
     algo.train()
@@ -52,10 +52,10 @@ run_experiment_lite(
     # Number of parallel workers for sampling
     n_parallel=1,
     # Only keep the snapshot parameters for the last iteration
-    snapshot_mode="last",
+    snapshot_mode='last',
     # Specifies the seed for the experiment. If this is not provided, a random seed
     # will be used
     seed=1,
-    plot=False,
+    plot=True,
     use_gpu=True
 )
