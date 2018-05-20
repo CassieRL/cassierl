06d13a81b1e278c160e4855a0d7c46af22c67416
Phi Luu <philectron@users.noreply.github.com>
2018-05-19 21:23:23 -0700

    Remove unused imports & Set step size to 1e-3

--- rllab/envs/vpg_cassie.py
+++ rllab/envs/vpg_cassie.py
@@ -1,11 +1,11 @@
-from rllab.algos.trpo import TRPO
-from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
-from cassie2d import Cassie2dEnv
-from rllab.envs.normalized_env import normalize
-from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
-from rllab.misc.instrument import run_experiment_lite
 import joblib
+
+from cassie2d import Cassie2dEnv
 from rllab.algos.vpg import VPG
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import run_experiment_lite
+from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 
 
 load_policy = False
@@ -24,7 +24,7 @@ def run_task(*_):
         policy = GaussianMLPPolicy(
             env_spec=env.spec,
             # The neural network policy should have two hidden layers, each with 128 hidden units.
-            hidden_sizes=(128, 128),
+            hidden_sizes=(32, 32),
             init_std=1.0,
             # adaptive_std=True,
         )
@@ -39,7 +39,7 @@ def run_task(*_):
         max_path_length=1000,  # dt = (1/2000)*n, where n is Step(n)
         n_itr=1000,
         discount=0.99,
-        step_size=0.005,      # default was 0.01
+        step_size=0.001,      # default was 0.01
         # Uncomment both lines (this and the plot parameter below) to enable plotting
         plot=True,
         use_gpu=True
